05a876aa318c522a3e297cf30e2d634c7ab086b5
dobrodelete <kirkon3004@gmail.com>
2024-05-20 22:44:13 +0300

    clear code

--- app/config.py
+++ app/config.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///site.db'
